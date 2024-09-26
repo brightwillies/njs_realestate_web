b99fe1a12e0599e2f9ca04c863ea11155621ecb1
brightwillies <bwboakye2@gmail.com>
2024-09-26 19:19:37 -0300

    Update apiRequest.js

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const apiRequest  = axios.create({
-    baseURL : "http://localhost:5000/api", 
+    baseURL : "https://njs-realestate-api.onrender.com/api", 
+    // baseURL : "http://localhost:5000/api", 
     withCredentials:true,
 })
 
